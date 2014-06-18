537aed07c8bd1be6753832c471b0262401e47550
Stephan Richter <srichter@srsoftware.de>
2014-06-18 12:33:23 +0200

    first few lines to open and close databsae

--- database.php
+++ database.php
@@ -1,5 +1,14 @@
 <?php
 
-require config/db.php
+  require 'config/db.php';
+
+  try {
+    $db = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
+  } catch (PDOException $pdoex) {
+    die($pdoex->getMessage());
+  }
+
+  print "databse opened"; 
+  $db=null; // close
 
 ?>
