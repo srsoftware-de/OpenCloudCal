84846d90f9ce3524ea75d1f27b9fadcbc098f888
Stephan Richter <srichter@srsoftware.de>
2014-06-19 11:59:30 +0200

    tag class copied and altered for urls

---
+++ include/class.url.php
@@ -0,0 +1,30 @@
+<?php
+	class url {
+		
+		/* loads a url, if the id is given (i.e. not 0), otherwise searches or creates the url by its text */ 
+		function __construct($id,$url=''){
+			global $db;
+			if ($id==0){ // no id given => search or create
+				$stm=$db->prepare("SELECT * FROM urls WHERE url=?");
+				$stm->execute(array($url));
+				$results=$stm->fetchAll();
+			
+				if ($results){
+					$this->id=$results[0]['uid'];
+				} else {
+					$stm=$db->prepare("INSERT INTO urls (url) VALUES (?)");
+					$stm->execute(array($url));
+					$this->id=$db->lastInsertId();
+				}
+				$this->text=$url;
+			} else { //id given => load
+				foreach ($db->query("SELECT url FROM urls WHERE uid=$id") as $row){
+					$this->url=$row['url'];
+					$this->id=$id;
+					break;
+				}
+			}
+		}
+	}
+	
+?>
