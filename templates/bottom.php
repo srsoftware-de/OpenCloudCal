b4297f972642eed070d5802edf0959ca0621543f
Stephan Richter <github@srsoftware.de>
2014-06-20 18:48:19 +0200

    added link to bottom line

--- templates/bottom.php
+++ templates/bottom.php
@@ -3,5 +3,6 @@
       echo '<div class="warnings">'.$warn.'</div>'.PHP_EOL;
     }
     ?>
+    OpenCloudCal 0.1 - Proudly developed using PHP and JavaScript. Find the sources at <a href="https://github.com/SRSoftware/OpenCloudCal">Github</a>.
   </body>
 </html>
