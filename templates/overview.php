388300232f19b904179ce988438aa0b8335aa6f3
Stephan Richter <github@srsoftware.de>
2014-06-20 18:41:46 +0200

    improved OSM link

--- templates/overview.php
+++ templates/overview.php
@@ -12,8 +12,12 @@ foreach ($appointments as $appointment){
 	$c=explode(',',$appointment->coords);
   print '<tr class="appointment">'.PHP_EOL;
   print '  <td class="datestart">'.$appointment->start.'</th>'.PHP_EOL;
-  print '  <td class="title"><a href=".?show='.$appointment->id.'">'.$appointment->title.'</a></th>'.PHP_EOL;
-  print '  <td class="location">'.$appointment->location.'<br/><a href="http://www.openstreetmap.org/?mlat='.$c[0].'&mlon='.$c[1].'&zoom=15">'.$appointment->coords.'</a></th>'.PHP_EOL;
+  print '  <td class="title"><a href=".?show='.$appointment->id.'">'.$appointment->title.'</a></th>'.PHP_EOL;  
+  print '  <td class="location">'.$appointment->location;
+  if (count($c)==2){
+		print '<br/><a href="http://www.openstreetmap.org/?mlat='.$c[0].'&mlon='.$c[1].'&zoom=15">'.$appointment->coords.'</a>';
+	}
+	print '</th>'.PHP_EOL;
   print '  <td class="tags">';
   foreach ($appointment->tags as $tag){
     print '<a href="?tag='.$tag->text.'">'.$tag->text.'</a> ';
