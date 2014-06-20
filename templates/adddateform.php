34bebdf8b317ea480d3fe4c5ba2e375228eff485
Stephan Richter <srichter@srsoftware.de>
2014-06-20 09:22:52 +0200

    started to implement basic datepicker

--- templates/adddateform.php
+++ templates/adddateform.php
@@ -1,4 +1,8 @@
+<?php
+  include 'forms.php';
+?>
 <form class="adddate" method="POST">
   <input type="text" name="description"/>Beschreibung<br/>
+    <?php datepicker('test'); ?>
   <input type="submit" value="Neuen Termin anlegen"/><br/>
 </form>
---
+++ templates/forms.php
@@ -0,0 +1,38 @@
+<?php
+  function datepicker($name,$time=null){
+    date_default_timezone_set('UTC');
+
+    if ($time==null){
+      $time=time();
+    }
+    print '<div class="datepicker">'.PHP_EOL;
+
+    $year=date('Y');
+    $selected_year=date('Y',$time);
+    print '  <select name="'.$name.'_year" size="1">'.PHP_EOL;
+    for ($i=0; $i<10; $i++){
+      if ($year==$selected_year){
+        print '    <option selected>'.$year.'</option>'.PHP_EOL;
+      } else {
+        print '    <option>'.$year.'</option>'.PHP_EOL;
+      }
+      $year+=1;
+    }
+    print '  </select>'.PHP_EOL;
+
+    $month=1;
+    $selected_month=date('m',$time);
+    print '  <select name="'.$name.'_month" size="1">'.PHP_EOL;
+    for ($i=0; $i<12; $i++){
+      if ($month==$selected_month){
+        print '    <option selected>'.$month.'</option>'.PHP_EOL;
+      } else {
+        print '    <option>'.$month.'</option>'.PHP_EOL;
+      }
+      $month+=1;
+    }
+    print '  </select>'.PHP_EOL;
+    print '</div>'.PHP_EOL;
+
+  }
+?>
