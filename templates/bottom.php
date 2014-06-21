00f4252fbff41af1e543b443f86bfa68f41b7b62
Stephan Richter <github@srsoftware.de>
2014-06-21 14:31:54 +0200

    increased version number

--- templates/bottom.php
+++ templates/bottom.php
@@ -2,7 +2,7 @@
     if (strlen($warnings)>3){
       echo '<div class="warnings">'.$warnings.'</div>'.PHP_EOL;
     }
-    echo 'OpenCloudCal 0.1 - '.str_replace('%link', 'https://github.com/SRSoftware/OpenCloudCal', loc('Proudly developed using PHP and JavaScript. Find the sources at <a href="%link">Github</a>.')); 
+    echo 'OpenCloudCal 0.2 - '.str_replace('%link', 'https://github.com/SRSoftware/OpenCloudCal', loc('Proudly developed using PHP and JavaScript. Find the sources at <a href="%link">Github</a>.')); 
     ?>
   </body>
 </html>
