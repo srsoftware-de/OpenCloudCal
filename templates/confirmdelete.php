5d7e27a84326da2a7b25f26fd858c9914e5eadc3
Stephan Richter <github@srsoftware.de>
2014-06-23 09:49:48 +0200

    added button class to confirmation options

--- templates/confirmdelete.php
+++ templates/confirmdelete.php
@@ -1,10 +1,7 @@
 <?php if ($format=='html') { ?>
 <div class="confirmation">
 <?php echo str_replace('%apptitle', $appointment->title, loc('Seriously, delete "%apptitle"?'));?><br/>
-<?php 
- echo '<a href="?delete='.$appointment->id.'&confirm=yes">'.loc('Yes').'</a>';
-?>
-&nbsp;&nbsp;&nbsp;
-<a href="."><?php echo loc('No')?></a>
+<a class="button red" href="?delete='<?php echo $appointment->id; ?>&confirm=yes"><?php echo loc('Yes'); ?></a>&nbsp;&nbsp;&nbsp;
+<a class="button green" href="."><?php echo loc('No')?></a>
 </div>
 <?php } ?>
