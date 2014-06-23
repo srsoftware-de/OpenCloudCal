d3dd92a3a0d21ff56f799ce04e5a3f7b1d1ee797
Stephan Richter <srichter@srsoftware.de>
2014-06-23 14:04:30 +0200

    fixed bug in templates/confirmdelete.php

--- templates/confirmdelete.php
+++ templates/confirmdelete.php
@@ -1,7 +1,7 @@
 <?php if ($format=='html') { ?>
 <div class="confirmation">
 <?php echo str_replace('%apptitle', $appointment->title, loc('Seriously, delete "%apptitle"?'));?><br/>
-<a class="button red" href="?delete='<?php echo $appointment->id; ?>&confirm=yes"><?php echo loc('Yes'); ?></a>&nbsp;&nbsp;&nbsp;
+<a class="button red" href="?delete=<?php echo $appointment->id; ?>&confirm=yes"><?php echo loc('Yes'); ?></a>&nbsp;&nbsp;&nbsp;
 <a class="button green" href="."><?php echo loc('No')?></a>
 </div>
 <?php } ?>
