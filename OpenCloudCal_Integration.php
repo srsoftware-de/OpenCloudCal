8e364c688cd99c5953a5a1328079094a795a92ef
Stephan Richter <github@srsoftware.de>
2014-07-08 11:12:17 +0200

    first test of replacement

--- OpenCloudCal_Integration.php
+++ OpenCloudCal_Integration.php
@@ -11,4 +11,24 @@ Update Server: http://srsoftware.de/
 Min WP Version: 1.5
 Max WP Version: 2.0.4
 */
+
+if (! function_exists('replace_open_cloudcal_tags')){
+	
+	function replace_open_cloudcal_tags($content){
+		$opencloudcal_content = $content;
+		$occ_pos=strpos($opencloudcal_content, 'opencloudcal:');
+		while (false !== $occ_pos){
+			$occ_end=strpos($opencloudcal_content, ' ');
+			$occ_key=substr($opencloudcal_content, $occ_pos,$occ_end-$occ_pos);
+			$opencloudcal_content=str_replace($occ_key,strtoupper($occ_key) , $opencloudcal_content);			
+		}
+		
+		return $opencloudcal_content;
+	} # function replace_open_cloudcal_tags()
+	
+	add_filter('the_content', 'replace_open_cloudcal_tags');	
+}
+
+
+
 ?>
