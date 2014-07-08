f396adf2e1cb969cac39d897167eaa2d6cec429c
Stephan Richter <github@srsoftware.de>
2014-07-08 10:50:44 +0200

    newly created OpenCloudCal_Integration.php

---
+++ OpenCloudCal_Integration.php
@@ -0,0 +1,14 @@
+
+<?php
+/*
+Plugin Name: OpenCloudCal Integration
+Plugin URI: http://cal.srsoftware.de/
+Description: The OpenCloudCal Integration plugin allows to integrate appointments listed in the Open Cloud Calendar by a tag.
+Version: 1.0
+Author: Stephan Richter
+Author URI: http://srsoftware.de/
+Update Server: http://srsoftware.de/
+Min WP Version: 1.5
+Max WP Version: 2.0.4
+*/
+?>
