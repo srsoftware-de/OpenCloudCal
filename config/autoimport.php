bdc2f9f3a981f1b3ecf49e91575f8369899f0972
Stephan Richter <github@srsoftware.de>
2015-04-28 14:59:39 +0200

    autoimport modified, so tags can be used

--- config/autoimport.php
+++ config/autoimport.php
@@ -1,4 +1,6 @@
 <?php
   $importurls=array(
   		'http://grical.org/s/?query=e&view=ical',
-  		'https://calcifer.datenknoten.me/all.ics');
+  		'https://calcifer.datenknoten.me/all.ics',
+		array('url' => 'https://www.google.com/calendar/ical/sfgnd1tl8n1fnkl0v3ne1oq9jc%40group.calendar.google.com/public/basic.ics','tag'=>'FreiRaum')
+);
