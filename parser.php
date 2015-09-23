d4b9f0639e13c46043d30584cd6422264ab00883
Stephan Richter <github@keawe.de>
2015-09-23 14:44:05 +0200

    working on parser for cafe wagner

--- parser.php
+++ parser.php
@@ -252,7 +252,9 @@ function parserImport($site,$tags=null,$coords=null){
 		$appointment=appointment::create($event_data['title'], $event_data['text'], $event_data['start'], $event_data['end'], $event_data['place'], $event_data['coords'],false);
 		$appointment->safeIfNotAlreadyImported($event_data['tags'],$event_data['links']);
 		foreach ($event_data['images'] as $src){
-			$appointment->addAttachment(parseAttachmentData($src));
+			$attach=array('aid'=>$appointment->id,'url'=>$src);
+			print_r($attach);
+			$appointment->addAttachment(parseAttachmentData($attach));
 		}
 	}
 }
