9a7a96cd9c559b2cd27f694ab0f9886d65d57ec9
Stephan Richter <github@keawe.de>
2015-09-23 15:45:44 +0200

    re-enabling ical-import

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ if (isset($_GET['autoimport']) && $_GET['autoimport']=='true'){
 			parserImport($item['url'],$item['tag'],$item['coords'],$item['location']);
 		} else parserImport($item);
 	}
-	die();
 	foreach ($ical_import_urls as $item){
 		if (is_array($item)){
 			importIcal($item['url'],$item['tag']);
