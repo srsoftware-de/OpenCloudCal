50b39144d4167f950f879e0f7b05e9bd888af035
Stephan Richter <github@keawe.de>
2016-04-28 12:01:24 +0200

    working on new Rosenkeller event parser

--- parsers/class.rosenkeller.php
+++ parsers/class.rosenkeller.php
@@ -1,8 +1,24 @@
 <?php
 class Rosenkeller{
-	private static $event_list_url = 'https://rosenkeller.org/programm.html';
+	private static $base_url = 'https://rosenkeller.org/';
+	private static $event_list_page = 'programm.html';
+	
 	public static function read_events(){
-		$xmlDoc = load_xml($event_list_url);
-		print_r($xmlDoc);		
+		$xml = load_xml(self::$base_url . self::$event_list_page);
+		$links = $xml->getElementsByTagName('a');
+		$event_pages = array();		
+		foreach ($links as $link){
+			$page = $link->getAttribute('href');
+			if (strpos($page, 'event_')===0) {
+				$event_pages[$page]=true; // used as keys, so duplicates get removed
+			}
+		}
+		foreach ($event_pages as $page => $dummy){
+			self::read_event(self::$base_url . $page);
+		}
+	}
+	
+	public static function read_event($source_url){
+		print $source_url.NL;
 	}
 }
