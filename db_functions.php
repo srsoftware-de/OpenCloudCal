93feee194b9fc7de2c3d3bad7c5a85729cff0213
Stephan Richter <s.richter@srsoftware.de>
2023-02-16 11:06:42 +0100

    fixed db charset
    Signed-off-by: Stephan Richter <s.richter@srsoftware.de>

--- db_functions.php
+++ db_functions.php
@@ -2,7 +2,7 @@
 /* this was written using http://code.tutsplus.com/tutorials/why-you-should-be-using-phps-pdo-for-database-access--net-12059 */
 function connectToDb($host,$database,$user,$pass){
 	try {
-		$db = new PDO("mysql:host=$host;dbname=$database", $user, $pass, array(PDO::ATTR_PERSISTENT => true)); // open db connection and cache it
+		$db = new PDO("mysql:host=$host;dbname=$database;charset=latin1", $user, $pass, array(PDO::ATTR_PERSISTENT => true)); // open db connection and cache it
 		//      print "databse opened\n";
 		return $db;
 	} catch (PDOException $pdoex) {
